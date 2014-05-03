193a3783ebec14ce371270acb356d933ebf8eeb2
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-03 21:33:42 +0900

    Create sequence OauthUrl

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,10 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { request =>
+    val newSession = getOrCreateSessionId()
+    val urlTwitter = getRedirectAction(request, newSession, "twitterClient", "/").getLocation()
+    val profile    = getUserProfile(request)
+    Ok(views.html.index(profile, urlTwitter)).withSession(newSession)
   }
-
 }
