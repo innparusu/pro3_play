91adf4a59b05828e88880c3f3306cbdf3d9cd398
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-04 00:52:35 +0900

    Fix controllers

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import org.pac4j.http.client._
+import org.pac4j.core.profile._
+import org.pac4j.play._
+import org.pac4j.play.scala._
+import play.api.libs.json.Json
 
-object Application extends Controller {
+object Application extends ScalaController {
 
   def index = Action { request =>
-    val newSession = getOrCreateSessionId()
-    val urlTwitter = getRedirectAction(request, newSession, "twitterClient", "/").getLocation()
+    val newSession = getOrCreateSessionId(request)
+    val urlTwitter = getRedirectAction(request, newSession, "TwitterClient", "/result").getLocation()
     val profile    = getUserProfile(request)
-    Ok(views.html.index(profile, urlTwitter)).withSession(newSession)
+    Ok(views.html.index(profile,urlTwitter)).withSession(newSession)
   }
 }
--- app/controllers/Global.scala
+++ app/controllers/Global.scala
@@ -2,12 +2,12 @@ import play.api._
 import play.api.Play.current
 import play.api.mvc._
 import org.pac4j.core.client.Clients
-import org.pac4j.oauth.client.TwitterClients
+import org.pac4j.oauth.client.TwitterClient
 import org.pac4j.play.Config
 
 object Global extends GlobalSettings {
   override def onStart(app:Application) {
-    val twitterClient = new TwitterClients()
+    val twitterClient = new TwitterClient("OEccWOt0t1FyfY5stIYKECME6", "ADqXLYdVMZkjf2yv4qhEpJGCau9pwLhmmzJOMFyU6im9XYX2IM")
     val clients = new Clients("http://localhost:9000/callback",twitterClient)
     Config.setClients(clients)
   }
