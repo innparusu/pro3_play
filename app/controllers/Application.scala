bf9e6bcc67b5c601099d2b471b3ced40de9c703b
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-07 15:42:10 +0900

    会話履歴を取得するメソッドを追加
    ```
    $ vim app/controllers/Application.scala
    ```

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import scala.collection.JavaConversions._
+import java.util.ArrayList
 import play.api._
 import play.api.mvc._
 import play.api.Play.current
@@ -23,10 +25,28 @@ object Application extends ScalaController {
 
   def result = Action { request =>
     val profile:TwitterProfile = getUserProfile(request).asInstanceOf[TwitterProfile]
-    val twitterApiKey = Play.application.configuration.getString("twitterApiKey").get
-    val twitterSecret = Play.application.configuration.getString("twitterSecret").get
-    var factory    = new TwitterFactory(new ConfigurationBuilder().setOAuthConsumerKey(twitterApiKey).setOAuthConsumerSecret(twitterSecret).build())
-    var twitter    = factory.getInstance(new AccessToken(profile.getAccessToken(), profile.getAccessSecret()));
-    Ok(views.html.result(twitter.getMentionsTimeline()))
+    val twitterApiKey      = Play.application.configuration.getString("twitterApiKey").get
+    val twitterSecret      = Play.application.configuration.getString("twitterSecret").get
+    var factory            = new TwitterFactory(new ConfigurationBuilder().setOAuthConsumerKey(twitterApiKey).setOAuthConsumerSecret(twitterSecret).build())
+    var twitter            = factory.getInstance(new AccessToken(profile.getAccessToken(), profile.getAccessSecret()));
+    var mentionsList       = twitter.getMentionsTimeline()
+
+    var status = mentionsList.get(0)
+    var list = List(status)
+    var conversationList = conversation(list, status, twitter)
+    for(status <- conversationList) {
+      println(status.getText())
+    }
+    Ok(views.html.result(mentionsList))
+  }
+
+  private def conversation (list:List[twitter4j.Status], status:twitter4j.Status, twitter:twitter4j.Twitter):List[twitter4j.Status] = {
+  var statusId = status.getInReplyToStatusId()
+    if (statusId  == -1) {
+      return list
+    }
+  var stat = twitter.showStatus(statusId)
+  var conversationList = stat::list 
+  conversation(conversationList, stat, twitter)
   }
 }
