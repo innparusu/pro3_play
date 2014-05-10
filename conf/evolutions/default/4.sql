85253d2fa571aed067ea0994a5cc90cb0bb0f38d
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-10 17:55:53 +0900

    Add evolutions file
    ```
    $ vim conf/evolutions/default/3.sql
    $ vim conf/evolutions/default/4.sql
    $ vim conf/evolutions/default/5.sql
    ```

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -2,7 +2,7 @@
 create table Mention (
   id int(10) not null auto_increment, 
   twitter_id  varchar(100)
-  mention_id  bigint,
+  mention_id  bigint(10),
   createDate timestamp default current_timestamp(),
 primary key(id));
 
@@ -13,6 +13,7 @@ create table User (
   access_secret varchar(100),
   createDate timestamp default current_timestamp(),
 primary key(id));
+
 # --- !Downs
-drop table User;
 drop table Mention;
+drop table User;
--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -1,8 +1,8 @@
 # --- !Ups
 create table Mention (
   id int(10) not null auto_increment, 
-  twitter_id  varchar(100)
-  mention_id  bigint,
+  twitter_id  varchar(100),
+  mention_id  bigint(10),
   createDate timestamp default current_timestamp(),
 primary key(id));
 
@@ -13,6 +13,6 @@ create table User (
   access_secret varchar(100),
   createDate timestamp default current_timestamp(),
 primary key(id));
+
 # --- !Downs
 drop table User;
-drop table Mention;
