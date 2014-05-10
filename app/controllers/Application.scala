c49f3509b1d862dd58bbf58dcc0a3928581f6d55
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-05-10 15:36:08 +0900

    Add login methods
    ```
    $ vim app/controllers/Application.scala
    ```

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,17 +20,12 @@ object Application extends ScalaController {
 
   def index = Action { request =>
     val session = getOrCreateSessionId(request)
-    val urlTwitter = getRedirectAction(request, session, "TwitterClient", "/result").getLocation()
+    val urlTwitter = getRedirectAction(request, session, "TwitterClient", "/signin").getLocation()
     Ok(views.html.index(urlTwitter)).withSession(session)
   } 
 
-  def result = Action { request =>
-    val profile            = getUserProfile(request).asInstanceOf[TwitterProfile]
-    val twitterApiKey      = Play.application.configuration.getString("twitterApiKey").get
-    val twitterSecret      = Play.application.configuration.getString("twitterSecret").get
-    var factory            = new TwitterFactory(new ConfigurationBuilder().setOAuthConsumerKey(twitterApiKey).setOAuthConsumerSecret(twitterSecret).build())
-    var twitter            = factory.getInstance(new AccessToken(profile.getAccessToken(), profile.getAccessSecret()));
-    var mentionsList       = twitter.getMentionsTimeline()
+  def signin = Action { request =>
+    val profile = getUserProfile(request).asInstanceOf[TwitterProfile]
 
     if (User.findByTwitterId(profile.getUsername()).isEmpty) {
       val user: User         = new User(twitter_id    = profile.getUsername(),
@@ -39,22 +34,55 @@ object Application extends ScalaController {
       User.insert(user)
     }
 
-    var status = mentionsList.get(0)
-    var list = List(status)
-    var conversationList = conversation(list, status, twitter)
-    for(status <- conversationList) {
-      println(status.getText())
-    }
-    Ok(views.html.result(conversationList))
+    Redirect("/result").withSession("twitter_id"    -> profile.getUsername())
   }
 
+  def result = Action { request =>
+    val twitter      = twitterTokenSet(request)
+    if (twitter == null) {
+      Redirect("/")
+    } else {
+      var mentionsList = twitter.getMentionsTimeline()
+      var status = mentionsList.get(0)
+      var list = List(status)
+      var conversationList = conversation(list, status, twitter)
+      for(status <- conversationList) {
+        println(status.getText())
+      }
+      Ok(views.html.result(conversationList))
+    }
+  }
+
+  def twitterTokenSet(request: RequestHeader) :twitter4j.Twitter = {
+    val user = currentUser(request)
+    if (user == null)  {
+      return null
+    }
+    val twitterApiKey      = Play.application.configuration.getString("twitterApiKey").get
+    val twitterSecret      = Play.application.configuration.getString("twitterSecret").get
+    var factory            = new TwitterFactory(new ConfigurationBuilder().setOAuthConsumerKey(twitterApiKey).setOAuthConsumerSecret(twitterSecret).build())
+    var twitter            = factory.getInstance(new AccessToken(user.access_token, user.access_secret))
+    twitter
+}
+
+  def currentUser(request: RequestHeader) :User = {
+    val sessionTwitterId   = request.session.get("twitter_id").getOrElse("")
+    if (sessionTwitterId == "") {
+      return null
+    }
+    val user               = User.findByTwitterId(sessionTwitterId).get
+    return user
+  }
+
+
+
   private def conversation (list: List[twitter4j.Status], status: twitter4j.Status, twitter: twitter4j.Twitter):List[twitter4j.Status] = {
-  var statusId = status.getInReplyToStatusId()
+    var statusId = status.getInReplyToStatusId()
     if (statusId  == -1) {
       return list
-    }
+  }
   var stat = twitter.showStatus(statusId)
   var conversationList = stat::list 
   conversation(conversationList, stat, twitter)
+    }
   }
-}
